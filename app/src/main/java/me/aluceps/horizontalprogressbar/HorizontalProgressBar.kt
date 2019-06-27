1342fe90ff08b517cff5e40cf2473855ff339d35
Atsushi Takano <aluceps@gmail.com>
2019-06-27 18:25:44 +0900

    Refactoring

--- app/src/main/java/me/aluceps/horizontalprogressbar/HorizontalProgressBar.kt
+++ app/src/main/java/me/aluceps/horizontalprogressbar/HorizontalProgressBar.kt
@@ -7,6 +7,7 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
+import java.util.*
 
 class HorizontalProgressBar @JvmOverloads constructor(
     context: Context?,
@@ -19,6 +20,12 @@ class HorizontalProgressBar @JvmOverloads constructor(
     private var colorBase = 0
     private var colorPrimary = 0
     private var colorSecondary = 0
+
+    private val innerLeft by lazy { 0 + borderWidth }
+    private val innerTop by lazy { 0 + borderWidth }
+    private val innerRight by lazy { width - borderWidth }
+    private val innerBottom by lazy { height - borderWidth }
+    private val innerRadius by lazy { cornerRadius - borderWidth }
 
     private val progressBase by lazy {
         Paint().apply {
@@ -40,13 +47,8 @@ class HorizontalProgressBar @JvmOverloads constructor(
         }
     }
 
-    private val rectBase by lazy {
-        RectF(0f, 0f, width.toFloat(), height.toFloat())
-    }
-
-    private val rectPrimary by lazy {
-        RectF(0f + borderWidth, 0f + borderWidth, width.toFloat() - borderWidth, height.toFloat() - borderWidth)
-    }
+    private val rectBase by lazy { RectF(0f, 0f, width.toFloat(), height.toFloat()) }
+    private val rectPrimary by lazy { RectF(innerLeft, innerTop, innerRight, innerBottom) }
 
     init {
         setup(context, attrs, defStyleAttr)
@@ -54,25 +56,33 @@ class HorizontalProgressBar @JvmOverloads constructor(
 
     private fun setup(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) {
         val typedArray = context?.obtainStyledAttributes(attrs, R.styleable.HorizontalProgressBar, defStyleAttr, 0)
-
-        typedArray?.getDimension(R.styleable.HorizontalProgressBar_progress_border_width, 0f)
-            ?.let { borderWidth = it }
-        typedArray?.getDimension(R.styleable.HorizontalProgressBar_progress_corner_radius, 0f)
-            ?.let { cornerRadius = it }
-        typedArray?.getColor(R.styleable.HorizontalProgressBar_progress_color_base, Color.GRAY)
-            ?.let { colorBase = it }
-        typedArray?.getColor(R.styleable.HorizontalProgressBar_progress_color_primary, Color.GRAY)
-            ?.let { colorPrimary = it }
-        typedArray?.getColor(R.styleable.HorizontalProgressBar_progress_color_secondary, Color.GRAY)
-            ?.let { colorSecondary = it }
-
+        typedArray?.let { t ->
+            t.getDimension(R.styleable.HorizontalProgressBar_progress_border_width, 0f)
+                .let { borderWidth = it }
+            t.getDimension(R.styleable.HorizontalProgressBar_progress_corner_radius, 0f)
+                .let { cornerRadius = it }
+            t.getColor(R.styleable.HorizontalProgressBar_progress_color_base, Color.GRAY)
+                .let { colorBase = it }
+            t.getColor(R.styleable.HorizontalProgressBar_progress_color_primary, Color.WHITE)
+                .let { colorPrimary = it }
+            t.getColor(R.styleable.HorizontalProgressBar_progress_color_secondary, Color.LTGRAY)
+                .let { colorSecondary = it }
+        }
         typedArray?.recycle()
+
+        Timer().apply {
+            schedule(object : TimerTask() {
+                override fun run() {
+                    post { invalidate() }
+                }
+            }, 10, 10)
+        }
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         if (canvas == null) return
         canvas.drawRoundRect(rectBase, cornerRadius, cornerRadius, progressBase)
-        canvas.drawRoundRect(rectPrimary, cornerRadius - borderWidth, cornerRadius - borderWidth, progressPrimary)
+        canvas.drawRoundRect(rectPrimary, innerRadius, innerRadius, progressPrimary)
     }
 }
